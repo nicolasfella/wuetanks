c51fb1b551fdde4119fe63fa3e015f6035aed2c1
nico <nico@nicoTV>
2016-10-24 23:50:27 +0200

    libGdx Projekt erstellt

---
+++ desktop/src/de/uniwuerzburg/battletanks/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package de.uniwuerzburg.battletanks.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import de.uniwuerzburg.battletanks.BattleTanks;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new BattleTanks(), config);
+	}
+}
