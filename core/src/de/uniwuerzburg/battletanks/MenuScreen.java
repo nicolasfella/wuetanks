825360e15389e1d26b9b4676f5c2d23ad0e3dfc3
Armin Bernstetter <armin.bernstetter@stud-mail.uni-wuerzburg.de>
2016-11-07 16:14:17 +0100

    Andere Startbutton Variante
    Ich hoffe das funktioniert. Der Branchname ist irgendwie vllt bisschen dumm :D

--- core/src/de/uniwuerzburg/battletanks/MenuScreen.java
+++ core/src/de/uniwuerzburg/battletanks/MenuScreen.java
@@ -3,22 +3,93 @@ package de.uniwuerzburg.battletanks;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.BitmapFont;
-import com.badlogic.gdx.graphics.g2d.TextureAtlas;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
-import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
+import com.badlogic.gdx.scenes.scene2d.ui.Button;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
-import com.badlogic.gdx.scenes.scene2d.ui.TextButton.TextButtonStyle;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
-import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
-import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
+import com.badlogic.gdx.utils.Align;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
 
+public class MenuScreen implements Screen {
+	final BattleTanks game;
+
+	private Skin skin;
+	private Stage stage;
+	private Table mainTable;
+
+	public MenuScreen(final BattleTanks game) {
+		this.game = game;
+
+		this.skin = new Skin(Gdx.files.internal("data/uiskin.json"));
+		this.stage = new Stage(new ScreenViewport());
+		Gdx.input.setInputProcessor(stage);
+
+		mainTable = new Table();
+		mainTable.setFillParent(true);
+
+		createStartButton();
+
+	}
+
+	@Override
+	public void show() {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void render(float delta) {
+		Gdx.gl.glClearColor(0, 0, 0.2f, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+		stage.act(Gdx.graphics.getDeltaTime());
+		stage.draw();
+	}
+
+	@Override
+	public void resize(int width, int height) {
+		stage.getViewport().update(width, height, true);
+	}
+
+	@Override
+	public void pause() {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void resume() {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void hide() {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void dispose() {
+		// TODO Auto-generated method stub
+	}
+
+	public void createStartButton() {
+		final Button start = new TextButton("START!", skin, "toggle");
+		mainTable.add(start);
+		start.align(Align.center);
+
+		start.addListener(new ChangeListener() {
+			@Override
+			public void changed(ChangeEvent event, Actor actor) {
+				game.setScreen(new GameScreen(game));
+				dispose();
+			}
+		});
+		stage.addActor(mainTable);
+	}
+
+}
+/*
 public class MenuScreen implements Screen {
 	
 	final BattleTanks game;
@@ -122,3 +193,4 @@ public class MenuScreen implements Screen {
 	
 
 }
+*/
