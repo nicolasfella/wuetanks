9d65010c5443d68b29dead0d9be0dfa8268c27e2
Stefan Ernst <stefan-ernst@hotmail.de>
2016-11-12 02:40:20 +0100

    filechooser wurde hinzugefügt und integriert in menuscreen, gamescreen hat ein neuen konstruktor der die tilemap übergeben kriegt

---
+++ core/src/de/uniwuerzburg/battletanks/FileChooser.java
@@ -0,0 +1,208 @@
+package de.uniwuerzburg.battletanks;
+
+import java.io.File;
+import java.io.FileFilter;
+import java.util.Comparator;
+
+import com.badlogic.gdx.Input.Keys;
+import com.badlogic.gdx.files.FileHandle;
+import com.badlogic.gdx.scenes.scene2d.Action;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Dialog;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.List;
+import com.badlogic.gdx.scenes.scene2d.ui.ScrollPane;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener.ChangeEvent;
+import com.badlogic.gdx.utils.Align;
+import com.badlogic.gdx.utils.Array;
+
+public class FileChooser extends Dialog {
+
+	public interface ResultListener {
+		boolean result(boolean success, FileHandle result);
+	}
+
+	private List<FileListItem> fileList;
+	private TextButton loadButton;
+	private TextButton cancelButton;
+	private ScrollPane scrollPane;
+	private String result;
+	private Skin skin;
+	private FileHandle currentDir;
+	private FileHandle baseDir;
+	private Label currentDirLabel;
+	private FileFilter filter = new FileFilter() {
+		@Override
+		public boolean accept(File pathname) {
+			String path = pathname.getPath();
+			return path.matches(".*\\.tmx");
+		}
+	};
+	public ResultListener resultListener = null;
+
+	// comparator für die korrekte sortierung der files
+
+	private Comparator<FileListItem> fileListComp = new Comparator<FileListItem>() {
+		@Override
+		public int compare(FileListItem file1, FileListItem file2) {
+			if (file1.file.isDirectory() && !file2.file.isDirectory()) {
+				return -1;
+			}
+			if (file1.file.isDirectory() && file2.file.isDirectory()) {
+				return 0;
+			}
+			if (!file1.file.isDirectory() && !file2.file.isDirectory()) {
+				return 0;
+			}
+			return 1;
+		}
+	};
+
+	public FileChooser(String title, Skin skin, FileHandle baseDir) {
+		super(title, skin);
+
+		this.skin = skin;
+		this.baseDir = baseDir;
+
+		Table table = getContentTable();
+		table.top().left();
+
+		currentDirLabel = new Label(baseDir.path(), skin);
+		currentDirLabel.setAlignment(Align.left);
+
+		fileList = new List<FileListItem>(skin);
+		fileList.getSelection().setProgrammaticChangeEvents(false);
+
+		loadButton = new TextButton("Load", skin);
+		button(loadButton, true);
+
+		cancelButton = new TextButton("Cancel", skin);
+		button(cancelButton, false);
+		key(Keys.ENTER, true);
+		key(Keys.ESCAPE, false);
+
+		fileList.addListener(new ChangeListener() {
+			@Override
+			public void changed(ChangeEvent event, Actor actor) {
+				FileListItem selected = fileList.getSelected();
+				if (!selected.file.isDirectory()) {
+					result = selected.file.name();
+				}
+			}
+		});
+
+	}
+
+	/**
+	 * wechselt auf das übergebene directory und aktualisiert die fileList
+	 *
+	 */
+
+	private void changeDirectory(FileHandle directory) {
+
+		currentDir = directory;
+		currentDirLabel.setText(currentDir.path());
+
+		Array<FileListItem> items = new Array<FileListItem>();
+
+		// hinzufügen aller directories
+
+		FileHandle[] list = directory.list();
+
+		for (FileHandle handle : list) {
+			if (handle.isDirectory()) {
+				items.add(new FileListItem(handle));
+			}
+		}
+
+		// hinzufügen aller dateien auf die der filter passt
+
+		list = directory.list(filter);
+
+		for (FileHandle handle : list) {
+			items.add(new FileListItem(handle));
+		}
+
+		items.sort(fileListComp);
+
+		if (directory.file().getParentFile() != null) {
+			items.insert(0, new FileListItem("..", directory.parent()));
+		}
+
+		fileList.setSelected(null);
+		fileList.setItems(items);
+	}
+
+	/**
+	 * erstellt aus dem aktuellen directory pfad und der markierten datei ein
+	 * FileHandle
+	 * 
+	 * @return
+	 */
+
+	public FileHandle getResult() {
+		String path = currentDir.path() + "/";
+		if (result != null && result.length() > 0) {
+			path += result;
+		}
+		return new FileHandle(path);
+	}
+
+	@Override
+	public Dialog show(Stage stage, Action action) {
+		Table content = getContentTable();
+		content.add(currentDirLabel).top().left().expandX().fillX().row();
+
+		scrollPane = new ScrollPane(fileList, skin);
+		scrollPane.setFadeScrollBars(false);
+		content.add(scrollPane).size(500, 400).fill().expand().row();
+
+		fileList.addListener(new ClickListener() {
+			@Override
+			public void clicked(InputEvent event, float x, float y) {
+				FileListItem selected = fileList.getSelected();
+				if (selected.file.isDirectory()) {
+					changeDirectory(selected.file);
+				}
+			}
+		});
+
+		changeDirectory(baseDir);
+
+		stage.setScrollFocus(scrollPane);
+		stage.setKeyboardFocus(scrollPane);
+
+		return super.show(stage, action);
+	}
+
+	public static FileChooser createDialog(String title, final Skin skin, final FileHandle path) {
+		FileChooser fileChooser = new FileChooser(title, skin, path) {
+			@Override
+			protected void result(Object object) {
+
+				if (resultListener == null) {
+					return;
+				}
+
+				final boolean success = (Boolean) object;
+				resultListener.result(success, getResult());
+			}
+		};
+
+		return fileChooser;
+
+	}
+
+	public FileChooser setResultListener(ResultListener result) {
+		this.resultListener = result;
+		return this;
+	}
+
+}
--- core/src/de/uniwuerzburg/battletanks/GameScreen.java
+++ core/src/de/uniwuerzburg/battletanks/GameScreen.java
@@ -3,6 +3,8 @@ package de.uniwuerzburg.battletanks;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.assets.loaders.resolvers.AbsoluteFileHandleResolver;
+import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
@@ -26,257 +28,256 @@ import java.util.List;
 
 public class GameScreen implements Screen {
 
-    static int width;
-    static int height;
-    final BattleTanks game;
+	static int width;
+	static int height;
+	final BattleTanks game;
 
+	SpriteBatch batch;
+	// Texture img;
+	BitmapFont font;
 
-    SpriteBatch batch;
-    //Texture img;
-    BitmapFont font;
+	OrthographicCamera camera;
+	Viewport viewPort;
 
-    OrthographicCamera camera;
-    Viewport viewPort;
+	private TiledMap tiledMap;
+	private TiledMapRenderer tiledMapRenderer;
+	private FileHandle tiledMapFileHandle;
 
-    private TiledMap tiledMap;
-    private TiledMapRenderer tiledMapRenderer;
+	private GlyphLayout layout;
 
-    private GlyphLayout layout;
+	private List<Entity> entities;
 
-    private List<Entity> entities;
+	private List<Player> players;
 
-    private List<Player> players;
+	public GameScreen(final BattleTanks game) {
+		this.game = game;
+	}
 
-    public GameScreen(final BattleTanks game) {
-        this.game = game;
-    }
-    
-    public GameScreen(final BattleTanks game, int time){
-    	this.game = game;
-    	System.out.println(time);
-    }
+	public GameScreen(final BattleTanks game, int time, FileHandle tiledMapFileHandle) {
+		this.game = game;
+		this.tiledMapFileHandle = tiledMapFileHandle;
+		System.out.println(time);
+	}
 
-    @Override
-    public void show() {
-        System.out.println("Start");
-        batch = new SpriteBatch();
-        // img = new Texture("gravel.jpg");
+	@Override
+	public void show() {
+		System.out.println("Start");
+		batch = new SpriteBatch();
+		// img = new Texture("gravel.jpg");
 
-        font = new BitmapFont();
-        font.setColor(Color.ORANGE);
+		font = new BitmapFont();
+		font.setColor(Color.ORANGE);
 
-        layout = new GlyphLayout();
-        layout.setText(font, "");
+		layout = new GlyphLayout();
+		layout.setText(font, "");
 
+		tiledMap = new TmxMapLoader(new AbsoluteFileHandleResolver()).load(tiledMapFileHandle.path());
+		tiledMapRenderer = new OrthogonalTiledMapRenderer(tiledMap);
+		MapProperties tiledMapProps = tiledMap.getProperties();
 
-        tiledMap = new TmxMapLoader().load("TestMap.tmx");
-        tiledMapRenderer = new OrthogonalTiledMapRenderer(tiledMap);
-        MapProperties tiledMapProps = tiledMap.getProperties();
+		// berechnung der fenstergröße durch die größe der tilemap
 
-        // berechnung der fenstergröße durch die größe der tilemap
+		int mapWidth = tiledMapProps.get("width", Integer.class);
+		int mapHeight = tiledMapProps.get("height", Integer.class);
+		int tilePixelWidth = tiledMapProps.get("tilewidth", Integer.class);
+		int tilePixelHeight = tiledMapProps.get("tileheight", Integer.class);
 
-        int mapWidth = tiledMapProps.get("width", Integer.class);
-        int mapHeight = tiledMapProps.get("height", Integer.class);
-        int tilePixelWidth = tiledMapProps.get("tilewidth", Integer.class);
-        int tilePixelHeight = tiledMapProps.get("tileheight", Integer.class);
+		width = mapWidth * tilePixelWidth;
+		height = mapHeight * tilePixelHeight;
 
-        width = mapWidth * tilePixelWidth;
-        height = mapHeight * tilePixelHeight;
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false, width, height);
+		viewPort = new FitViewport(width, height, camera);
 
+		entities = new ArrayList<Entity>();
+		players = new ArrayList<Player>(4);
 
-        camera = new OrthographicCamera();
-        camera.setToOrtho(false, width, height);
-        viewPort = new FitViewport(width, height, camera);
+		Player player1 = new Player(Input.Keys.W, Input.Keys.S, Input.Keys.A, Input.Keys.D, Input.Keys.E);
+		entities.add(player1);
+		player1.setPosition(0, 0);
+		players.add(player1);
 
-        entities = new ArrayList<Entity>();
-        players = new ArrayList<Player>(4);
-
-        Player player1 = new Player(Input.Keys.W, Input.Keys.S, Input.Keys.A, Input.Keys.D, Input.Keys.E);
-        entities.add(player1);
-        player1.setPosition(0, 0);
-        players.add(player1);
-
-        Player player2 = new Player(Input.Keys.UP, Input.Keys.DOWN, Input.Keys.LEFT, Input.Keys.RIGHT, Input.Keys.E);
-        entities.add(player2);
-        player2.setPosition(150, 500);
-        players.add(player2);
+		Player player2 = new Player(Input.Keys.UP, Input.Keys.DOWN, Input.Keys.LEFT, Input.Keys.RIGHT, Input.Keys.E);
+		entities.add(player2);
+		player2.setPosition(150, 500);
+		players.add(player2);
 
 		/*
-         * Player player3 = new Player(Input.Keys.U, Input.Keys.J, Input.Keys.H,
+		 * Player player3 = new Player(Input.Keys.U, Input.Keys.J, Input.Keys.H,
 		 * Input.Keys.K, Input.Keys.E); entities.add(player3);
 		 * player3.setPosition(400, 300); players.add(player3);
 		 */
 
+		// einlesen der objects aus dem objects layer der tilemap und erstellung
+		// der obstacles
 
-        // einlesen der objects aus dem objects layer der tilemap und erstellung der obstacles
+		MapLayer objectsLayer = tiledMap.getLayers().get("objects");
 
-        MapLayer objectsLayer = tiledMap.getLayers().get("objects");
+		if (objectsLayer != null) {
+			for (MapObject object : objectsLayer.getObjects()) {
 
-        for (MapObject object : objectsLayer.getObjects()) {
+				if (object instanceof RectangleMapObject) {
+					RectangleMapObject rectObject = (RectangleMapObject) object;
+					Rectangle rect = rectObject.getRectangle();
+					Obstacle obst = new Obstacle();
+					obst.setPosition((int) rect.getX(), (int) rect.getY());
+					obst.setWidth((int) rect.getWidth());
+					obst.setHeight((int) rect.getHeight());
+					entities.add(obst);
+				}
+			}
+		}
 
-            if (object instanceof RectangleMapObject) {
-                RectangleMapObject rectObject = (RectangleMapObject) object;
-                Rectangle rect = rectObject.getRectangle();
-                Obstacle obst = new Obstacle();
-                obst.setPosition((int) rect.getX(), (int) rect.getY());
-                obst.setWidth((int) rect.getWidth());
-                obst.setHeight((int) rect.getHeight());
-                entities.add(obst);
-            }
-        }
+	}
 
+	@Override
+	public void render(float delta) {
 
-    }
+		Gdx.graphics.setTitle("Battletanks " + Gdx.graphics.getFramesPerSecond() + " fps");
 
-    @Override
-    public void render(float delta) {
+		Gdx.gl.glClearColor(0, 0, 0, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-        Gdx.graphics.setTitle("Battletanks "+Gdx.graphics.getFramesPerSecond()+" fps");
+		camera.update();
 
-        Gdx.gl.glClearColor(0, 0, 0, 1);
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		for (Entity entity : entities) {
+			entity.update();
+		}
 
-        camera.update();
+		// Player to player collision
+		// extend to player-obstacle?
+		for (int i = 0; i < entities.size(); i++) {
+			for (int j = 0; j < entities.size(); j++) {
+				if (i == j)
+					continue;
 
-        for (Entity entity : entities) {
-            entity.update();
-        }
+				Entity e1 = entities.get(i);
+				Entity e2 = entities.get(j);
 
-        // Player to player collision
-        // extend to player-obstacle?
-        for (int i = 0; i < entities.size(); i++) {
-            for (int j = 0; j < entities.size(); j++) {
-                if (i == j)
-                    continue;
+				if (e1 instanceof Player) {
 
-                Entity e1 = entities.get(i);
-                Entity e2 = entities.get(j);
+					if (e2 instanceof Player || e2 instanceof Obstacle) {
+						checkCollisionPlayerObstacle((Player) e1, e2);
+					}
 
-                if (e1 instanceof Player) {
+				}
 
-                    if (e2 instanceof Player || e2 instanceof Obstacle) {
-                        checkCollisionPlayerObstacle((Player) e1, e2);
-                    }
+			}
+		}
 
-                }
+		tiledMapRenderer.setView(camera);
+		tiledMapRenderer.render();
 
-            }
-        }
+		batch.setProjectionMatrix(camera.combined);
+		batch.begin();
 
+		for (Entity entity : entities) {
+			entity.render(batch);
+		}
 
-        tiledMapRenderer.setView(camera);
-        tiledMapRenderer.render();
+		// font.draw(batch, "Player 1: 7 hits 3 kills", 10, 20);
+		/*
+		 * font.draw(batch, "Player 3: 3 hits 3 kills", 10, 25);
+		 * font.draw(batch, "Player 1: 5 hits 8 kills", 10, height - 10);
+		 * layout.setText(font, "Player 2: 5 hits 8 kills"); font.draw(batch,
+		 * "Player 2: 5 hits 8 kills", width - layout.width - 10, height - 10);
+		 * layout.setText(font, "Player 4: 10 hits 6 kills"); font.draw(batch,
+		 * "Player 4: 10 hits 6 kills", width - layout.width - 10, 25);
+		 */
 
+		batch.end();
 
-        batch.setProjectionMatrix(camera.combined);
-        batch.begin();
+	}
 
-        for (Entity entity : entities) {
-            entity.render(batch);
-        }
+	private void checkCollisionPlayerObstacle(Player p, Entity o) {
 
-        // font.draw(batch, "Player 1: 7 hits 3 kills", 10, 20);
-          /*font.draw(batch, "Player 3: 3 hits 3 kills", 10, 25);
-		  font.draw(batch, "Player 1: 5 hits 8 kills", 10, height - 10);
-		  layout.setText(font, "Player 2: 5 hits 8 kills"); font.draw(batch,
-		  "Player 2: 5 hits 8 kills", width - layout.width - 10, height - 10);
-		  layout.setText(font, "Player 4: 10 hits 6 kills"); font.draw(batch,
-		  "Player 4: 10 hits 6 kills", width - layout.width - 10, 25);*/
+		Rectangle r1 = new Rectangle(p.getX(), p.getY(), p.getWidth(), p.getHeight());
+		Rectangle r2 = new Rectangle(o.getX(), o.getY(), o.getWidth(), o.getHeight());
 
-        batch.end();
+		if (r1.overlaps(r2)) {
+			// System.out.println("Intersection");
+		}
 
+		// p1 is left to p2
+		if (p.getX() + p.getWidth() >= o.getX()) {
+			if (p.getOldPosition().x + p.getWidth() <= o.getX()) {
+				if (p.getY() + p.getHeight() > o.getY() && p.getY() < o.getY() + o.getHeight()) {
+					if (p.getX() > p.getOldPosition().x) {
+						p.setX(o.getX() - p.getWidth());
+						p.getSpeed().x = 0;
+						System.out.println("Kollision von links");
+					}
+				}
+			}
+		}
 
-    }
+		// p1 is below p2
+		if (p.getY() + p.getHeight() >= o.getY()) {
+			if (p.getOldPosition().y + p.getHeight() <= o.getY()) {
+				if (p.getX() + p.getWidth() > o.getX() && p.getX() < o.getX() + o.getWidth()) {
+					if (p.getY() > p.getOldPosition().y) {
+						p.setY(o.getY() - p.getHeight());
+						p.getSpeed().y = 0;
+						System.out.println("Kollision von unten");
+					}
+				}
+			}
+		}
 
-    private void checkCollisionPlayerObstacle(Player p, Entity o) {
+		// p1 is above p2
+		if (p.getY() < o.getY() + o.getHeight()) {
+			if (p.getOldPosition().y >= o.getY() + o.getHeight()) {
+				if (p.getX() + p.getWidth() > o.getX() && p.getX() < o.getX() + o.getWidth()) {
+					if (p.getY() < p.getOldPosition().y) {
+						p.setY(o.getY() + o.getHeight());
+						p.getSpeed().y = 0;
+						System.out.println("Kollision von oben");
+					}
+				}
+			}
+		}
 
-        Rectangle r1 = new Rectangle(p.getX(), p.getY(), p.getWidth(), p.getHeight());
-        Rectangle r2 = new Rectangle(o.getX(), o.getY(), o.getWidth(), o.getHeight());
+		// p is right to o
+		if (p.getX() <= o.getX() + o.getWidth()) {
+			if (p.getOldPosition().x >= o.getX() + o.getWidth()) {
+				if (p.getY() + p.getHeight() > o.getY() && p.getY() < o.getY() + o.getHeight()) {
+					if (p.getX() < p.getOldPosition().x) {
+						p.setX(o.getX() + o.getWidth());
+						p.getSpeed().x = 0;
+						System.out.println("Kollision von rechts");
+					}
+				}
+			}
+		}
 
-        if (r1.overlaps(r2)) {
-            //System.out.println("Intersection");
-        }
+	}
 
-        // p1 is left to p2
-        if (p.getX() + p.getWidth() >= o.getX()) {
-            if (p.getOldPosition().x + p.getWidth() <= o.getX()) {
-                if (p.getY() + p.getHeight() > o.getY() && p.getY() < o.getY() + o.getHeight()) {
-                    if (p.getX() > p.getOldPosition().x) {
-                        p.setX(o.getX() - p.getWidth());
-                        p.getSpeed().x = 0;
-                        System.out.println("Kollision von links");
-                    }
-                }
-            }
-        }
+	@Override
+	public void resize(int width, int height) {
+		viewPort.update(width, height);
+	}
 
-        // p1 is below p2
-        if (p.getY() + p.getHeight() >= o.getY()) {
-            if (p.getOldPosition().y + p.getHeight() <= o.getY()) {
-                if (p.getX() + p.getWidth() > o.getX() && p.getX() < o.getX() + o.getWidth()) {
-                    if (p.getY() > p.getOldPosition().y) {
-                        p.setY(o.getY() - p.getHeight());
-                        p.getSpeed().y = 0;
-                        System.out.println("Kollision von unten");
-                    }
-                }
-            }
-        }
+	@Override
+	public void pause() {
 
-        // p1 is above p2
-        if (p.getY() < o.getY() + o.getHeight()) {
-            if (p.getOldPosition().y >= o.getY() + o.getHeight()) {
-                if (p.getX() + p.getWidth() > o.getX() && p.getX() < o.getX() + o.getWidth()) {
-                    if (p.getY() < p.getOldPosition().y) {
-                        p.setY(o.getY() + o.getHeight());
-                        p.getSpeed().y = 0;
-                        System.out.println("Kollision von oben");
-                    }
-                }
-            }
-        }
+	}
 
-        // p is right to o
-        if (p.getX() <= o.getX() + o.getWidth()) {
-            if (p.getOldPosition().x >= o.getX() + o.getWidth()) {
-                if (p.getY() + p.getHeight() > o.getY() && p.getY() < o.getY() + o.getHeight()) {
-                    if (p.getX() < p.getOldPosition().x) {
-                        p.setX(o.getX() + o.getWidth());
-                        p.getSpeed().x = 0;
-                        System.out.println("Kollision von rechts");
-                    }
-                }
-            }
-        }
+	@Override
+	public void resume() {
 
-    }
+	}
 
-    @Override
-    public void resize(int width, int height) {
-        viewPort.update(width, height);
-    }
+	@Override
+	public void hide() {
 
-    @Override
-    public void pause() {
+	}
 
-    }
+	@Override
+	public void dispose() {
+		font.dispose();
+		batch.dispose();
+		tiledMap.dispose();
 
-    @Override
-    public void resume() {
-
-    }
-
-    @Override
-    public void hide() {
-
-    }
-
-    @Override
-    public void dispose() {
-        font.dispose();
-        batch.dispose();
-        tiledMap.dispose();
-
-    }
+	}
 
 }
