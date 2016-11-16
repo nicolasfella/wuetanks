8ecb818fc24a3cb6ece5cbf43684843c179dd0ad
Nicolas Fella <nicolas.fella@gmx.de>
2016-11-17 00:02:24 +0100

    Mit Dokumentation begonnen

--- core/src/de/uniwuerzburg/battletanks/Entity.java
+++ core/src/de/uniwuerzburg/battletanks/Entity.java
@@ -1,10 +1,8 @@
 package de.uniwuerzburg.battletanks;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.math.Vector2;
 
 /**
@@ -12,19 +10,47 @@ import com.badlogic.gdx.math.Vector2;
  */
 public class Entity {
 
+    /**
+     * The position of the entity
+     */
     protected Vector2 position;
 
+    /**
+     * The position of the entity in the previous frame
+     */
     protected Vector2 oldPosition;
-    protected int width, height;
 
+    /**
+     * The width of the entity
+     */
+    protected int width;
+
+    /**
+     * The height of the entity
+     */
+    protected int height;
+
+    /**
+     * The speed of the entity, in units per second
+     */
     protected Vector2 speed;
 
+    /**
+     * The texture of the entity, loaded from the TextureAtlas
+     */
     protected Sprite sprite;
 
+    /**
+     * Creates an entity at (0,0) with an empty texture
+     */
     public Entity() {
         this("empty");
     }
 
+    /**
+     * Creates an entity at (0,0) with given texture
+     * @param spriteName the name of the texture in the TextureAtlas
+     */
     public Entity(String spriteName) {
 
         position = new Vector2();
@@ -35,11 +61,18 @@ public class Entity {
         sprite.setPosition(position.x, position.y);
     }
 
+    /**
+     * Used for game logic, called each frame
+     */
     public void update() {
         oldPosition = position.cpy();
         position.add(speed.scl(Gdx.graphics.getDeltaTime()));
     }
 
+    /**
+     * Draws the entity to the given SpriteBatch
+     * @param batch The SpriteBatch the entity is drawn to
+     */
     public void render(SpriteBatch batch) {
         sprite.setPosition(position.x, position.y);
         sprite.setSize(width, height);
